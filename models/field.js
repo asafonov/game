d8f179b59602d0c7ea92b92c66fe0964ac2b13e6
asafonov <me@asafonov.org>
2020-02-14 11:17:51 +0300

    Added field model and started view

--- models/field.js
+++ models/field.js
@@ -1,4 +1,24 @@
 var Field = function() {
   this.width = 64;
   this.height = 64;
+  var _hero = null;
+  var _positions = ['moveUp', 'moveDown', 'moveLeft', 'moveRight'];
+
+  this.setHero = function (hero) {
+    _hero = hero;
+  }
+
+  this.correctPosition = function (obj) {
+    obj.position.x = Math.min(obj.position.x, this.width - 1);
+    obj.position.y = Math.min(obj.position.y, this.height - 1);
+    obj.position.x = Math.max(obj.position.x, 0);
+    obj.position.y = Math.max(obj.position.y, 0);
+  }
+
+  for (var i = 0; i < _positions.length; ++i) {
+    this[_positions[i]] = function (obj) {
+      obj[_positions[i]]();
+      this.correctPosition(obj);
+    }
+  }
 }
---
+++ view/field.js
@@ -0,0 +1,7 @@
+var FieldView = function() {
+
+  this.element = document.createElement('div');
+  this.element.id = 'field';
+  document.body.appendChild(this.element);
+
+}
